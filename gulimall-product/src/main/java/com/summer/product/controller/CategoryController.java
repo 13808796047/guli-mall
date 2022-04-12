8b05d5b471f70f8063ebb56e3ec29d1862fee0fa
Summer <511391805@qq.com>
2022-04-13 00:29:44 +0800

    :ok_hand:商品服务-API-三级分类-网关统一配置跨域

--- gulimall-product/src/main/java/com/summer/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/summer/product/controller/CategoryController.java
@@ -1,20 +1,14 @@
 package com.summer.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.summer.common.utils.R;
 import com.summer.product.entity.CategoryEntity;
 import com.summer.product.service.CategoryService;
-import com.summer.common.utils.PageUtils;
-import com.summer.common.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -31,13 +25,12 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 查出所有分类以及子分类，以树形结构组装
      */
-    @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
-
-        return R.ok().put("page", page);
+    @RequestMapping("/list/tree")
+    public R list(@RequestParam Map<String, Object> params) {
+        List<CategoryEntity> entities = categoryService.listWithTree();
+        return R.ok().put("categories", entities);
     }
 
 
@@ -45,8 +38,8 @@ public class CategoryController {
      * 信息
      */
     @RequestMapping("/info/{catId}")
-    public R info(@PathVariable("catId") Long catId){
-		CategoryEntity category = categoryService.getById(catId);
+    public R info(@PathVariable("catId") Long catId) {
+        CategoryEntity category = categoryService.getById(catId);
 
         return R.ok().put("category", category);
     }
@@ -55,8 +48,8 @@ public class CategoryController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody CategoryEntity category){
-		categoryService.save(category);
+    public R save(@RequestBody CategoryEntity category) {
+        categoryService.save(category);
 
         return R.ok();
     }
@@ -65,8 +58,8 @@ public class CategoryController {
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody CategoryEntity category){
-		categoryService.updateById(category);
+    public R update(@RequestBody CategoryEntity category) {
+        categoryService.updateById(category);
 
         return R.ok();
     }
@@ -75,8 +68,8 @@ public class CategoryController {
      * 删除
      */
     @RequestMapping("/delete")
-    public R delete(@RequestBody Long[] catIds){
-		categoryService.removeByIds(Arrays.asList(catIds));
+    public R delete(@RequestBody Long[] catIds) {
+        categoryService.removeByIds(Arrays.asList(catIds));
 
         return R.ok();
     }
--- gulimall-product/src/main/java/com/summer/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/summer/product/service/impl/CategoryServiceImpl.java
@@ -1,16 +1,19 @@
 package com.summer.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.summer.common.utils.PageUtils;
 import com.summer.common.utils.Query;
-
 import com.summer.product.dao.CategoryDao;
 import com.summer.product.entity.CategoryEntity;
 import com.summer.product.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service("categoryService")
@@ -26,4 +29,33 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return new PageUtils(page);
     }
 
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 查出所有分类
+        List<CategoryEntity> categories = baseMapper.selectList(null);
+        // 组成父子的树形结构
+        // 1.找到所有的一级分类
+        List<CategoryEntity> parentCategory = categories.stream()
+                .filter(category -> category.getParentCid() == 0)
+                .map(category -> {
+                    category.setChildren(getChildrens(category, categories));
+                    return category;
+                })
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
+        return parentCategory;
+    }
+
+    private List<CategoryEntity> getChildrens(CategoryEntity category, List<CategoryEntity> categories) {
+        List<CategoryEntity> children = categories.stream().filter(item ->
+                        item.getParentCid().equals(category.getCatId())
+                )
+                .map(item -> {
+                    item.setChildren(getChildrens(item, categories));
+                    return item;
+                })
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
+        return children;
+    }
 }
